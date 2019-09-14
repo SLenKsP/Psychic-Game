298549155b608d5fa1befe2f11f5fc403dc5d723
slenksp <slenksp@github.com>
2019-09-14 17:39:22 -0500

    hide the computer guess at web page level (still can be seen at console though)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
 // Variables
 var chars = "abcdefghijklmnopqrstuvwxyz";
 var guess = chars.charAt(Math.floor(Math.random() * chars.length));
-// var totalGuess = 10;
+console.log(guess); //just to test a game (open dev.tool to look at the computer guess)
 var win = 0;
 var loss = 0;
 var remainingGuess = 10;
 var userText = document.getElementById("myGuess");
 
-document.getElementById("compGuess").innerHTML = guess;
+// document.getElementById("compGuess").innerHTML = guess;
 document.getElementById("remainingGuesses").innerHTML = remainingGuess;
 
 // Reset function - this will reset remaining guesses and comp guess
 function reset() {
   guess = chars.charAt(Math.floor(Math.random() * chars.length));
   //   document.getElementById("compGuess").innerHTML = guess;
+  document.getElementById("guessHistory").innerHTML = " ";
   remainingGuess = 10;
   document.getElementById("remainingGuesses").innerHTML = remainingGuess;
   userText.innerHTML = " ";
@@ -22,40 +23,42 @@ function reset() {
 // DOM values for compGuess and remainingGuesses
 
 // Key up function
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
   userText.textContent = event.key;
   document.getElementById(
     "guessHistory"
   ).innerHTML += `${userText.innerHTML} , `; //adding user key input to guess history
-
+  var lowerCase = userText.innerHTML.toLowerCase();
   // comparing user selected char to comp char
-  if (userText.innerHTML === guess) {
+  if (lowerCase === guess) {
     win++;
     // remainingGuess--;
     document.getElementById("totalWins").innerHTML = win;
+
     document.getElementById("guessHistory").innerHTML = " ";
     alert(`Letter was ${guess}`); //alerts correct char
     reset();
-    document.getElementById("compGuess").innerHTML = reset();
+    console.log(guess);
+    // document.getElementById("compGuess").innerHTML = reset();
   } else {
     remainingGuess--;
     document.getElementById("remainingGuesses").innerHTML = remainingGuess;
-
   }
   // function to increment loss if no remaining guesses left
   if (remainingGuess === 0) {
     loss++;
+
     document.getElementById("totalLosses").innerHTML = loss;
     alert(`Correct letter was ${guess}`);
-    document.getElementById("guessHistory").innerHTML = " ";
-    remainingGuess = 10;
-    document.getElementById("remainingGuesses").innerHTML = remainingGuess;
+    reset();
+    console.log(guess);
   }
 };
 
 function masterReset() {
   guess = chars.charAt(Math.floor(Math.random() * chars.length));
-  document.getElementById("compGuess").innerHTML = guess;
+  console.log(guess);
+  // document.getElementById("compGuess").innerHTML = guess;
   remainingGuess = 10;
   document.getElementById("remainingGuesses").innerHTML = remainingGuess;
   win = 0;
@@ -65,3 +68,5 @@ function masterReset() {
   userText.innerHTML = " ";
   document.getElementById("guessHistory").innerHTML = " ";
 }
+
+//animation for win and loss
